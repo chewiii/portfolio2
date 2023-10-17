779e8f28da1d83385e1bad631ff1c9a37cfa7bf2
chewiii <134231147+chewiii@users.noreply.github.com>
2023-10-18 01:57:44 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_z6jt2cb";
+      const templateID = "template_ooe4n2l";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
+  
+  
+  
+          var typed = new Typed('#auto-type', {
+          strings: ['Programmer', 'Web Developer', 'Tech Geek'],
+          typeSpeed: 100,
+          backSpeed: 100,
+          loop: true
+          });
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
